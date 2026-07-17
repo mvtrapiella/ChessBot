a7b39724abb623bc55d88c8eae43f2c78b27aaa1
uo300652 <uo300652@uniovi.es>
2026-07-17 02:06:17 +0200

    Add initial module for the board folder and the declaration of the main types: the color and the cell states (the piece number and the no in pasant number)

---
+++ board_backend/src/board/mod.rs
@@ -0,0 +1,7 @@
+// Declare the submodules (this tells Rust to compile types.rs and state.rs)
+pub mod state;
+pub mod types;
+
+// Re-export the main items to make imports much cleaner in main.rs
+pub use state::Board;
+pub use types::Color;
---
+++ board_backend/src/board/types.rs
@@ -0,0 +1,23 @@
+#[derive(Copy, Clone, Debug, PartialEq)]
+#[repr(u8)]
+pub enum Color {
+    White = 0,
+    Black = 1,
+}
+
+pub const EMPTY: u8 = 0;
+pub const WHITE_PAWN: u8 = 1;
+pub const WHITE_ROOK: u8 = 2;
+pub const WHITE_KNIGHT: u8 = 3;
+pub const WHITE_BISHOP: u8 = 4;
+pub const WHITE_QUEEN: u8 = 5;
+pub const WHITE_KING: u8 = 6;
+pub const BLACK_PAWN: u8 = 7;
+pub const BLACK_ROOK: u8 = 8;
+pub const BLACK_KNIGHT: u8 = 9;
+pub const BLACK_BISHOP: u8 = 10;
+pub const BLACK_QUEEN: u8 = 11;
+pub const BLACK_KING: u8 = 12;
+
+pub const NO_SQUARE: u8 = 64;
+
