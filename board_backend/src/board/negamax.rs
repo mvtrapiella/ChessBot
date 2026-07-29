c006d6ad0bdd5d17704010d216c9d3624fb9a370
uo300652 <uo300652@uniovi.es>
2026-07-29 18:19:55 +0200

    create the generator for the random keys for zobrian hashing for the 12 piece types, the 4 possible castle right and the 8 columns where a possible en_passant capture could be

--- board_backend/src/board/negamax.rs
+++ board_backend/src/board/negamax.rs
@@ -1,4 +1,3 @@
-use crate::board::Board;
 use crate::board::position::Position;
 use crate::board::types::Move;
 
---
+++ board_backend/src/board/zobric.rs
@@ -0,0 +1,10 @@
+use crate::board::Board;
+
+impl Board{
+    pub fn calculate_zobric_hash(&self) -> u64{
+        let result = 0u64;
+        
+
+        result
+    }
+}
